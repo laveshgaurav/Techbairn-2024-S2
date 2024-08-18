820428b56c246aaac64b784ed7762c24a68990fd
Lavesh Gaurav <39463503+laveshgaurav@users.noreply.github.com>
2024-08-18 20:39:00 +0530

    Day13

--- e-commerce-frontend/src/App.tsx
+++ e-commerce-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import Button from "./components/Button";
 
 function App() {
@@ -7,7 +7,31 @@ function App() {
   // };
 
   // states -Hook
+  console.log("App Is Running");
   const [userName, setUsername] = useState<string>("Hello");
+
+  type T_UserInput = {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  };
+
+  const [userInput, setUserInput] = useState<T_UserInput>({
+    firstName: "",
+    middleName: "",
+    lastName: "",
+  });
+
+  const [checked, setChecked] = useState<boolean>(true);
+
+  const [userList, setUserList] = useState([]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserInput((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const [like, setLike] = useState<number>(0);
 
@@ -23,22 +47,73 @@ function App() {
     setLike((prev) => prev + 1);
   };
 
-  // UseEffect Hook
+  const fetchData = async () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setUserList(json);
+      });
+  };
+
+  useEffect(() => {
+    console.log("In Use Effect");
+    updateUserName();
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <Button text={`Like ${like}`} onClick={UpdateLike} type="Solid" />
+      <Button text={`Like ${like}`} onFetchData={UpdateLike} type="Solid" />
       <h1>{userName}</h1>
       <Button
         text="Update Name"
         type="Outline"
-        onClick={() => updateUserName()}
+        onFetchData={() => updateUserName()}
       />
-      <Button text="Check Name" type="Solid" onClick={() => checkUserName()} />
+      <Button
+        text="Check Name"
+        type="Solid"
+        onFetchData={() => checkUserName()}
+      />
 
       {/* Forms */}
+      <input
+        value={userInput.firstName}
+        placeholder="Enter First Name"
+        onChange={handleInputChange}
+        name="firstName"
+      />
+      {/* checkbox */}
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+      />
 
       {/* Data Fetching */}
+
+      <h1>User List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.map(
+            (data: { name: string; email: string; phone: string }, index) => (
+              <tr key={index}>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+                <td>{data.phone}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
--- e-commerce-frontend/src/components/Button.tsx
+++ e-commerce-frontend/src/components/Button.tsx
@@ -1,12 +1,12 @@
 type Props = {
   text: string;
   type: "Solid" | "Outline";
-  onClick: (id: string) => void;
+  onFetchData: (id: string) => void;
 };
 
-function Button({ text = "Hello", type, onClick }: Props) {
+function Button({ text = "Hello", type, onFetchData }: Props) {
   return (
-    <button className={type} onClick={() => onClick("8909953490")}>
+    <button className={type} onClick={() => onFetchData("8909953490")}>
       {text}
     </button>
   );
